51b5250a0ccb29bc0bdd014ee0260b9a90aa624a
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2021-10-06 10:00:10 -0300

    Js now shows the inputed digits on the calc display and stores then for calculation. Also, when an operator is selected the display clears when the next number digit is inserted. The calculations are almost working. There is a bug when making subsequent calculations using different operators.

--- script.js
+++ script.js
@@ -1,30 +1,51 @@
-let displayText = document.querySelector(".display input")
+let numberA;
+let numberB;
+let currentOperator;
+
+let displayText = document.querySelector(".display");
+displayText.textContent = "";
 
 const digits = document.querySelectorAll(".digits div button");
 digits.forEach(digit => {
     digit.addEventListener("click", () => {
-        inputDisplay()
+        if (currentOperator !== undefined) {
+            displayText.textContent = "" + digit.innerHTML
+        } else displayText.textContent += digit.innerHTML;
     });
 });
 
-function inputDisplay() {
-    console.log("Funciona")
-}
+const operators = document.querySelectorAll(".operators div button");
+operators.forEach(operator => {
+    operator.addEventListener("click", () => {
+        if (numberA === undefined) {
+            numberA = parseFloat(displayText.innerHTML);
+            currentOperator = operator.innerHTML;
+        } else {
+            numberB = parseFloat(displayText.innerHTML);
+            operate(numberA, currentOperator, numberB);
+            numberA = parseFloat(displayText.textContent);
+        };
+    });
+});
 
 function add(a, b) {
-    return a + b;
+    displayText.textContent = a + b;
+    return;
 };
 
 function subtract(a, b) {
-    return a - b;
+    displayText.textContent = a - b;
+    return;
 };
 
 function multiply(a, b) {
-    return a * b;
+    displayText.textContent = a * b;
+    return;
 };
 
 function divide(a, b) {
-    return a / b;
+    displayText.textContent = a / b;
+    return;
 };
 
 function operate(a, operator, b) {
